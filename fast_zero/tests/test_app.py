e867451490379818f899cbb7c42937074f5854fc
breno.oliveira <brenooalves2003@gmail.com>
2026-02-10 15:43:10 -0300

    :tada: new get current user function

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -7,7 +7,7 @@ def test_get_token(client, user):
     response = client.post('/token', data={'username': user.email, 'password': user.clean_password})
 
     token = response.json()
-
+    assert response.status_code == HTTPStatus.OK
     assert 'access_token' in token
     assert 'token_type' in token
 
